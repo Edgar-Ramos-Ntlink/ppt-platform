503808402c57cefd27e313caf48aeabb9816954b
Edgar Ramos <eddypeacegamer@gmail.com>
2022-01-03 22:52:52 -0600

    Adding prod cancel validation

--- invoice-manager-services/src/main/java/com/business/unknow/services/services/executor/NtinkExecutorService.java
+++ invoice-manager-services/src/main/java/com/business/unknow/services/services/executor/NtinkExecutorService.java
@@ -71,11 +71,11 @@ public class NtinkExecutorService extends AbstractPackExecutor {
               context.getFacturaDto().getRfcEmisor(),
               context.getFacturaDto().getRfcRemitente(),
               expresion);
-      // if (glocalConfigs.getEnvironment().equals("prod")) {
-      client
-          .getNtlinkClient(ntlinkProperties.getHost(), ntlinkProperties.getContext())
-          .cancelar(requestModel);
-      // }
+      if (glocalConfigs.getEnvironment().equals("prod")) {
+        client
+            .getNtlinkClient(ntlinkProperties.getHost(), ntlinkProperties.getContext())
+            .cancelar(requestModel);
+      }
       context.getFacturaDto().setStatusFactura(FacturaStatusEnum.CANCELADA.getValor());
       context.getFacturaDto().setFechaCancelacion(new Date());
       return context;
