717b36f330c07c20b395aa6996bdfd1d6866584d
Cristian <escamilla.cristian@gmail.com>
2022-04-28 15:56:06 -0600

    Update CfdiService.java

--- invoice-manager-services/src/main/java/com/business/unknow/services/services/CfdiService.java
+++ invoice-manager-services/src/main/java/com/business/unknow/services/services/CfdiService.java
@@ -65,14 +65,14 @@ public class CfdiService {
     recalculateCfdiAmmounts(cfdi);
     Comprobante comprobante = cfdiMapper.cfdiToComprobante(cfdi);
     filesService.sendXmlToS3(cfdi.getFolio(), comprobante);
+    createReportData(cfdi);
     // TODO ADD CREATE PDF
-    // TODO ADD REPORT CONCEPTOS BDD
     return cfdiMapper.comprobanteToCfdi(comprobante);
   }
 
   public Cfdi updateCfdiBody(String folio, Cfdi cfdi) throws InvoiceManagerException {
+    deleteReportData(cfdi);
     // TODO ADD CREATE PDF
-    // TODO ADD REPORT CONCEPTOS BDD
     return insertNewCfdi(cfdi);
   }
 
