3ba40cc0e98163c45efcb5108ac05fad41b741ea
Edgar Ramos <edgar.ramos@ntlink.com>
2022-05-26 20:55:00 -0500

    Removing code

--- invoice-manager-ui/src/app/@core/data/cfdi-data.ts
+++ invoice-manager-ui/src/app/@core/data/cfdi-data.ts
@@ -1,11 +1,6 @@
 import { Observable } from 'rxjs';
 import { Cfdi } from '../models/cfdi/cfdi';
-import { Pago } from '../models/cfdi/pago';
-import { Factura } from '../models/factura';
 
 export abstract class CfdiData {
-    abstract getCfdiByFolio(prefolio: string): Observable<Cfdi>;
-    abstract updateCfdi(cfdi: Cfdi): Observable<Cfdi>;
     abstract recalculateCfdi(cfdi: Cfdi): Observable<Cfdi>;
-    abstract getFacturaInfo(prefolio: number): Observable<Factura>;
 }
