d1f31f14ba7058bd4fc32a433559e2bf2ba699c9
Avelino <hugo.avelino@ntlink.com.mx>
2022-06-07 23:01:41 -0400

    Enable block functionality and fix company table headers

--- invoice-manager-ui/src/app/models/empresa.ts
+++ invoice-manager-ui/src/app/models/empresa.ts
@@ -7,6 +7,7 @@ export class Empresa {
     public activo: boolean;
     public estatus: string; // Se calculara automaticamnete basado entre representanteLegal & correo & cert & key & cuentas
     public operativa: boolean;
+    public bloqueada: boolean;
     public giro: string;
     public tipo: string; // linea empresa
     public regimenFiscal: string;
